df188a6d86b666382c4dba92ab1fabc8d28546ff
artartur <arturgfs@hotmail.com>
2023-07-18 01:46:12 -0300

    Create strings file

---
+++ src/utils/strings.ts
@@ -0,0 +1,8 @@
+export const HeaderMenuOptions = {
+  About: "Sobre nos",
+  Announcement: "Anunciar Pet",
+  Contacts: "Contatos",
+  Home: "Inicio",
+  Logo: "Pet Connect",
+  Rescue: "Resgatar",
+};
