502d9694b6e4c66b79024414a13b2648ca6be9b7
artartur <arturgfs@hotmail.com>
2023-07-27 02:11:48 -0300

    Add the Outlet component

--- src/screens/App.tsx
+++ src/screens/App.tsx
@@ -1,10 +1,14 @@
-import Header from "../components/Header";
+import React from "react";
+import { Outlet } from "react-router-dom";
+
 import Footer from "../components/Footer";
+import Header from "../components/Header";
 function App() {
   return (
     <>
       <div>
         <Header />
+        <Outlet />
         <Footer />
       </div>
     </>
