7293621a2794a6fec848cbe44672569f4dfb9284
artartur <arturgfs@hotmail.com>
2023-07-27 02:07:06 -0300

    Create Error screen

---
+++ src/screens/Error.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "../styles/error.scss";
+
+export default function Error() {
+  return (
+    <div className="container-error">
+      <h1>Pagina não encontrada! &#9785;</h1>
+    </div>
+  );
+}
