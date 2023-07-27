d6eadfcf53cfa880b9b2a8208329116be6081a43
artartur <arturgfs@hotmail.com>
2023-07-27 02:04:56 -0300

    Change anchors to Link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,7 @@
+import React from "react";
 import "../styles/header.scss";
+import { Link } from "react-router-dom";
+
 import { HeaderMenuOptions } from "../utils/strings";
 
 export default function Header() {
@@ -6,28 +9,28 @@ export default function Header() {
     <header>
       <div className="container">
         <div>
-          <a href="#">
+          <Link to="/">
             <h2>{HeaderMenuOptions.Logo}</h2>
-          </a>
+          </Link>
         </div>
         <nav>
           <ul>
             <li>
-              <a href="#">{HeaderMenuOptions.Home}</a>
+              <Link to="/">{HeaderMenuOptions.Home}</Link>
             </li>
             <li>
-              <a href="#">{HeaderMenuOptions.Rescue}</a>
+              <Link to="/resgatar">{HeaderMenuOptions.Rescue}</Link>
             </li>
             <li>
-              <a href="#">{HeaderMenuOptions.About}</a>
+              <Link to="/">{HeaderMenuOptions.About}</Link>
             </li>
             <li>
-              <a href="#">{HeaderMenuOptions.Contacts}</a>
+              <Link to="/contatos">{HeaderMenuOptions.Contacts}</Link>
             </li>
           </ul>
         </nav>
         <div className="announcement-button">
-          <a href="#">{HeaderMenuOptions.Announcement}</a>
+          <Link to="/anunciar">{HeaderMenuOptions.Announcement}</Link>
         </div>
       </div>
     </header>
